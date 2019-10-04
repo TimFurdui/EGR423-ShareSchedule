8face0a1f86168227e157639b2a67d9c882c5c64
PeterTimothyFurdui <PeterTimothyFurdui@gmail.com>
2019-10-03 19:03:13 -0700

    First Commit

---
+++ app/src/main/java/com/egr423/egr423_shareschedule/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.egr423.egr423_shareschedule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
