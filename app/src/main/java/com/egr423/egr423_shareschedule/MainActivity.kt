ca56fa081b273f39766f85de8aa730979a31a23f
PeterTimothyFurdui <PeterTimothyFurdui@gmail.com>
2019-10-10 17:06:03 -0700

    User-Reg

--- app/src/main/java/com/egr423/egr423_shareschedule/MainActivity.kt
+++ app/src/main/java/com/egr423/egr423_shareschedule/MainActivity.kt
@@ -2,11 +2,31 @@ package com.egr423.egr423_shareschedule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var loginButton : Button
+    private lateinit var registerButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginButton = findViewById(R.id.login_button)
+        registerButton = findViewById(R.id.register_button)
+
+        loginButton.setOnClickListener {
+
+            //TODO GO TO THE LOGIN FRAGMENT
+
+        }
+
+        loginButton.setOnClickListener {
+
+            //TODO GO TO THE REGISTER FRAGMENT
+
+        }
     }
 }
---
+++ app/src/main/java/com/egr423/egr423_shareschedule/User.kt
@@ -0,0 +1,5 @@
+package com.egr423.egr423_shareschedule
+
+import java.util.*
+
+data class User (var userId: Int, var firstName: String, var lastName: String, var email: String, var password: String, var birthDate: Date)
