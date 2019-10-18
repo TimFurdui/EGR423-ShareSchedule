3ee7945924cc0a3773cdb3981751339ba7142d80
PeterTimothyFurdui <PeterTimothyFurdui@gmail.com>
2019-10-17 19:08:58 -0700

    added view.xml and updated view files

--- app/src/main/java/com/egr423/egr423_shareschedule/User.kt
+++ app/src/main/java/com/egr423/egr423_shareschedule/User.kt
@@ -2,4 +2,7 @@ package com.egr423.egr423_shareschedule
 
 import java.util.*
 
-data class User (var userId: Int, var firstName: String, var lastName: String, var email: String, var password: String, var birthDate: Date)
+data class User (/*@PrimaryKey val userId: UUID = UUID.randomUUID()*/var userId: Int , var firstName: String, var lastName: String, var email: String, var password: String, var birthDate: Date, var userPrivilege: Int, var userComments: String)
+
+
+//TODO FIRE BASE HAS A EMAIL REGISTRATION OPTION!!!!!!!!!!!!!!!
