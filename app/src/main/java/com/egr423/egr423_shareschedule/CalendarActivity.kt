3aee5d77bc838cb705a4556a88acafaf88c2f58a
Nate2187 <Petertimothy.furdui@calbaptist.edu>
2019-10-26 18:46:54 -0700

    Working on Calendar Activity

---
+++ app/src/main/java/com/egr423/egr423_shareschedule/CalendarActivity.kt
@@ -0,0 +1,31 @@
+package com.egr423.egr423_shareschedule
+
+import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class CalendarActivity : AppCompatActivity(){
+
+    private lateinit var calendar : CalendarView
+    private lateinit var name : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+
+        name = findViewById(R.id.nameOfUser)
+        name.text = "temporary text"
+
+        calendar = findViewById(R.id.calendarView)
+        calendar.setOnDateChangeListener{view, year, month, dayOfMonth ->
+            val msg = "Selected date is" + dayOfMonth + "/" + (month + 1) + "/" + year
+            Toast.makeText(this@CalendarActivity, msg, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+}
