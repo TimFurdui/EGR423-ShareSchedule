9911253cff467ced96d377deb56181ecfba170e0
Nate2187 <Petertimothy.furdui@calbaptist.edu>
2019-10-26 18:46:54 -0700

    Working on Calendar Activity

--- app/src/main/java/com/egr423/egr423_shareschedule/LoginActivity.kt
+++ app/src/main/java/com/egr423/egr423_shareschedule/LoginActivity.kt
@@ -7,7 +7,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+
 import android.widget.Toast
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -28,6 +28,8 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var loginEmail: EditText
     private lateinit var loginPassword: EditText
     private lateinit var loginButton: Button
+
+    public lateinit var firebaseUser : FirebaseUser
 
     // [START declare_auth]
     private lateinit var auth: FirebaseAuth
@@ -69,6 +71,8 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
+
+        //TODO Handle checking if user signed in
 //        updateUI(currentUser)
     }
     // [END on_start_check_user]
@@ -84,11 +88,13 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                 // Google Sign In was successful, authenticate with Firebase
                 val account = task.getResult(ApiException::class.java)
                 firebaseAuthWithGoogle(account!!)
+
+                Log.w(TAG, "Google sign in SUCCEEDED")
             } catch (e: ApiException) {
                 // Google Sign In failed, update UI appropriately
                 Log.w(TAG, "Google sign in failed", e)
                 // [START_EXCLUDE]
-//                updateUI(null)
+                updateUI(null)
                 // [END_EXCLUDE]
             }
         }
@@ -106,12 +112,21 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithCredential:success")
                     val user = auth.currentUser
+
+                    //TODO MAKE A NEW INTENT TO PASS DATA FROM ONE ACTIVITY TO ANOTHER
+                    //TODO PASS USER DATA TO CALENDARACTIVITY AND POPULATE NAME FIELD
+                    if (user != null) {
+                        firebaseUser = user
+
+                    }
+                    //TODO if Login is successful than redirect to calendar activity
+                    startActivity(Intent(this, CalendarActivity::class.java))
 //                    updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
                     Toast.makeText(this, "AUTH FAILED", Toast.LENGTH_LONG).show()
-//                    updateUI(null)
+                    updateUI(null)
                 }
 
             }
@@ -125,41 +140,33 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     }
     // [END signin]
 
-//    private fun signOut() {
-//        // Firebase sign out
-//        auth.signOut()
-//
-//        // Google sign out
-//        googleSignInClient.signOut().addOnCompleteListener(this) {
-//            updateUI(null)
-//        }
-//    }
+    private fun signOut() {
+        // Firebase sign out
+        auth.signOut()
 
-//    private fun revokeAccess() {
-//        // Firebase sign out
-//        auth.signOut()
-//
-//        // Google revoke access
-//        googleSignInClient.revokeAccess().addOnCompleteListener(this) {
-//            updateUI(null)
-//        }
-//    }
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener(this) {
+            updateUI(null)
+        }
+    }
 
-//    private fun updateUI(user: FirebaseUser?) {
-//        if (user != null) {
-//            status.text = getString(R.string.google_status_fmt, user.email)
-//            detail.text = getString(R.string.firebase_status_fmt, user.uid)
-//
-//            signInButton.visibility = View.GONE
-//            signOutAndDisconnect.visibility = View.VISIBLE
-//        } else {
-//            status.setText(R.string.signed_out)
-//            detail.text = null
-//
-//            signInButton.visibility = View.VISIBLE
-//            signOutAndDisconnect.visibility = View.GONE
-//        }
-//    }
+    private fun revokeAccess() {
+        // Firebase sign out
+        auth.signOut()
+
+        // Google revoke access
+        googleSignInClient.revokeAccess().addOnCompleteListener(this) {
+            updateUI(null)
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            Toast.makeText(this, "Welcome " + auth.currentUser?.displayName, Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Invalid user information, please try again.", Toast.LENGTH_LONG).show()
+        }
+    }
 
     override fun onClick(v: View) {
         val i = v.id
@@ -171,7 +178,8 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     companion object {
-        private const val TAG = "GoogleActivity"
+        private const val TAG = "LoginActivity"
         private const val RC_SIGN_IN = 9001
     }
+
 }
--- app/src/main/java/com/egr423/egr423_shareschedule/MainActivity.kt
+++ app/src/main/java/com/egr423/egr423_shareschedule/MainActivity.kt
@@ -26,14 +26,11 @@ class MainActivity : AppCompatActivity() {
 
         loginButton.setOnClickListener {
             //TODO GO TO THE LOGIN ACTIVITY
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-
+            startActivity(Intent(this, LoginActivity::class.java))
         }
         registerButton.setOnClickListener {
             //TODO GO TO THE REGISTER FRAGMENT
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
 
 
