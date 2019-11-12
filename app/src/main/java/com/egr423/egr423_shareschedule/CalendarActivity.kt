8f5d65119dcebef2456a85575b23b63999b2b504
RigobertoVincent <Rigobertovicente.Gonzalez@calbaptist.edu>
2019-11-11 16:15:04 -0800

    dailyview

--- app/src/main/java/com/egr423/egr423_shareschedule/CalendarActivity.kt
+++ app/src/main/java/com/egr423/egr423_shareschedule/CalendarActivity.kt
@@ -6,23 +6,7 @@ import android.widget.CalendarView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.api.services.calendar.model.CalendarList
 import java.util.*
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.api.services.calendar.Calendar
-import com.google.api.services.calendar.model.Event
-import com.google.api.services.calendar.model.Events
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
-
-
-
 
 class CalendarActivity : AppCompatActivity(){
 
@@ -37,13 +21,11 @@ class CalendarActivity : AppCompatActivity(){
 
         name.text = intent.extras!!["LoginActivity.nameOfUser"] as CharSequence?
 
-//        calendar = findViewById(R.id.calendarView)
-//        calendar.setOnDateChangeListener{view, year, month, dayOfMonth ->
-//            val msg = "Selected date is" + dayOfMonth + "/" + (month + 1) + "/" + year
-//            Toast.makeText(this@CalendarActivity, msg, Toast.LENGTH_SHORT).show()
-//        }
-//        val feed = FirebaseAuth.getInstance().currentUser.
-
+        calendar = findViewById(R.id.calendarView)
+        calendar.setOnDateChangeListener{view, year, month, dayOfMonth ->
+            val msg = "Selected date is" + dayOfMonth + "/" + (month + 1) + "/" + year
+            Toast.makeText(this@CalendarActivity, msg, Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
