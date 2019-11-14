70355799b13dde120da3d5be32355dbcaec4fcb1
Nate2187 <Petertimothy.furdui@calbaptist.edu>
2019-11-13 22:28:08 -0800

    began on dailyView

--- app/src/main/java/com/egr423/egr423_shareschedule/User.kt
+++ app/src/main/java/com/egr423/egr423_shareschedule/User.kt
@@ -5,8 +5,3 @@ import java.util.*
 
 @IgnoreExtraProperties
 data class User (var firstName: String? = null, var lastName: String? = null, var email: String? = null, var password: String? = null, var birthDate: Date? = null, var userPrivilege: Int? = null/*, var userComments: Array<String>? = null*/)
-
-
-
-
-//TODO FIRE BASE HAS A EMAIL REGISTRATION OPTION!!!!!!!!!!!!!!!
