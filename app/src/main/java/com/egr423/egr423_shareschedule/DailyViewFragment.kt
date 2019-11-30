4b4a0dfa910970c34bcf7679641795ccb0bb1613
RigobertoVincent <Rigobertovicente.Gonzalez@calbaptist.edu>
2019-11-30 07:51:51 -0800

    DailyView

--- app/src/main/java/com/egr423/egr423_shareschedule/DailyViewFragment.kt
+++ app/src/main/java/com/egr423/egr423_shareschedule/DailyViewFragment.kt
@@ -1,19 +1,19 @@
 package com.egr423.egr423_shareschedule
 
-import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-var array = arrayOf("Test", "Test1", "Test2")
 
+var array = arrayOf("Test","Test1","Test2")
 
 class DailyViewFragment : Fragment() {
 
 
     override fun onCreateView(
-        inflater: LayoutInflater?,
+        inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
